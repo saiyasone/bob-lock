46e1b4be010a28214475a0a3bbaa9078d648d8fc
lien teng <olienteng895@gmail.com>
2024-03-05 13:00:59 +0700

    upadte

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+
 import axios from "axios";
 
 function Home() {
@@ -6,6 +7,10 @@ function Home() {
     request: "",
     second: "",
   });
+  const [progress, setProgress] = useState(0);
+  const [currentDate, setCurrentDate] = useState("");
+  const cancelTokenSource = axios.CancelToken.source();
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleApi() {
     try {
@@ -26,15 +31,55 @@ function Home() {
   }
 
   async function insertData() {
-    // console.log(formField);
-    // let interval = setInterval(() => {
-    for (let index = 0; index < 10000000; index++) {
-      console.log(index + 1);
-    }
-    // }, 1000);
+    const startTime = new Date();
+    try {
+      setIsLoading(true);
+      // console.log(formField);
+      // let interval = setInterval(() => {
+      for (let index = 0; index < formField.request || 1; index++) {
+        await axios.post(
+          "http://192.168.100.4:5000/api/v1/user",
+          {
+            id: index + 1,
+            one: "1",
+            two: "2",
+            three: "3",
+            four: "4",
+            five: "5",
+            six: "6",
+            seven: "7",
+            eight: "8",
+            nine: "9",
+          },
+          {
+            onUploadProgress: ({ loaded, total }) => {
+              const percent = Math.round((loaded * 100) / total);
+              setProgress(percent);
+            },
+            cancelToken: cancelTokenSource.token,
+          }
+        );
+      }
 
-    // clearInterval(interval);
-    // 192.168.100.4
+      const endTime = new Date();
+      const elapsedTime = endTime - startTime;
+      setCurrentDate(elapsedTime.toString());
+      // }, 1000);
+
+      // clearInterval(interval);
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log("Request is cancelled", error);
+      } else {
+        console.log("Something went wrong ", error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleCancelToken() {
+    cancelTokenSource.cancel("Request is cancelled by user.");
   }
 
   function handleChange(e) {
@@ -55,33 +100,41 @@ function Home() {
   return (
     <div className="Home">
       <div className="container">
+        <p>time used: {currentDate}</p>
         <div>
           <input
             name="request"
-            type="text"
+            type="number"
             placeholder="Request time"
             onChange={(e) => handleChange(e)}
           />
         </div>
-        <div>
+        {/* <div>
           <input
             name="second"
             type="text"
             placeholder="Second time"
             onChange={(e) => handleChange(e)}
           />
-        </div>
-        <div>
-          <input
-            name="Data"
-            type="text"
-            placeholder="Data"
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
+        </div> */}
 
-        <div className="mt-3">
-          <button onClick={insertData}>Test api</button>
+        <div
+          className="mt-3"
+          style={{ display: "flex", alignItems: "center", gap: "10px" }}
+        >
+          <button
+            disabled={formField.request ? false : true}
+            onClick={insertData}
+          >
+            Test api
+          </button>
+
+          <button
+            disabled={!isLoading ? true : false}
+            onClick={handleCancelToken}
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </div>
